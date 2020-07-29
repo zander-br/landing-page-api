40ccd4cc8f6602270d57cb9ce376e0224a0d59bc
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-07-29 08:43:30 -0300

    chore: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '628dd5154563429b49e3fb442591cff2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '37960886-b71b-4374-aee9-5163f60fe69b'
+};
